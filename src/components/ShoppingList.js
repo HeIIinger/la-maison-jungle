01f54361e91abeb047f384dd0f9c3cc5e3f3d83f
Théo Litzler <theolitzler@gmail.com>
2022-01-29 21:57:59 -0500

    P2C4

---
+++ src/components/ShoppingList.js
@@ -0,0 +1,32 @@
+import '../styles/ShoppingList.css'
+import { plantList } from '../datas/plantList'
+import CareScale from './CareScale'
+
+function ShoppingList() {
+    const categories = plantList.reduce(
+        (acc, plant) => acc.includes(plant.category) ? acc : acc.concat(plant.category), []
+    )
+
+    return (
+        <div>
+            <ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map((plant) => (
+                    <li key={plant.id} className='lmj-plant-item'>
+                        {plant.name}
+                        { plant.isBestSale && <div className='lmj-sales'></div> }
+                        <CareScale careType='water' scaleValue={plant.water} />
+                        <CareScale careType='light' scaleValue={plant.light} />
+                    </li>
+                ))}
+			</ul>
+        </div>
+        
+    )
+}
+
+export default ShoppingList
